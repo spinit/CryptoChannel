1879646acc1a7bac991a58adbfd163cccab0e2fa
Ermanno Astolfi <ermanno.astolfi@gmail.com>
2016-11-28 17:04:35 +0100

    fix test Signed-off-by:Ermanno Astolfi <ermanno.astolfi@gmail.com>

--- autoload.php
+++ autoload.php
@@ -4,3 +4,11 @@ ob_start();
 $util = include_once __DIR__.'/lib/Util.php';
 
 spl_autoload_register(array($util, 'autoload'));
+
+function array_get($array, $field, $default = '')
+{
+    if (!isset($array[$field])) {
+        return $default;
+    }
+    return $array[$field];
+}
