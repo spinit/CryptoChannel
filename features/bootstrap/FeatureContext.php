4c93c38f2f487d5803540b67ca460e4c6bb5c5d1
Ermanno Astolfi <ermanno.astolfi@gmail.com>
2016-12-19 10:51:30 +0100

    fix test Signed-off-by:Ermanno Astolfi <ermanno.astolfi@gmail.com>

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -2,5 +2,28 @@
  
 class FeatureContext extends Behat\MinkExtension\Context\MinkContext
 {
- 
+    /**
+     * @Given /^imposto "([^"]*)" con "([^"]*)"$/
+     */
+    public function impostoCon($arg1, $arg2)
+    {
+        print_r(get_class_methods($this->getSession()->getDriver()));
+    }
+
+    /**
+     * @When /^clicco su "([^"]*)"$/
+     */
+    public function cliccoSu($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then /^casella "([^"]*)" ha valore "([^"]*)"$/
+     */
+    public function casellaHaValore($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
 }
--- features/test1.feature
+++ features/test1.feature
@@ -2,6 +2,6 @@
 Feature: Test di prova
 
     Scenario: Prova del test
-        Given imposto "dati" con "test"
+        Given imposto "message" con "test"
         When clicco su "invia"
-        Then casella "result" ha valore "Recuperato [ test]"
+        Then casella "response" ha valore "Ricevuto [test]"
