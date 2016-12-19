da821cc0feb33704030e41469c2d1e1c9e4328a1
Ermanno Astolfi <ermanno.astolfi@gmail.com>
2016-12-19 20:03:07 +0100

    fix ricevuto Signed-off-by:Ermanno Astolfi <ermanno.astolfi@gmail.com>

--- demo/index.php
+++ demo/index.php
@@ -24,7 +24,7 @@ if (isset($_GET['echo'])) {
     try {
         
         $data = $channelServer->unpack($content);
-        $message = "Ricevuto [{$data}]{$_SERVER['SERVER_PORT']}";
+        $message = "Ricevuto [{$data}]";
         
         if ($_GET['echo']>0) {
             list($base, ) = explode('?', $_SERVER['REQUEST_URI']);
